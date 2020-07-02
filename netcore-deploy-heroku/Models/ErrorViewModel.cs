eec137a8c1ae884a29d5dc67d299faf6322863b2
JBravo <bravo.jose.luis18@gmail.com>
2020-07-01 22:38:40 -0300

    Add project files.

---
+++ netcore-deploy-heroku/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcore_deploy_heroku.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
