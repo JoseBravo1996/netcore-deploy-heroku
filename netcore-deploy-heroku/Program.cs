8356183232d95f2c6a7063ad376da85606b31747
JBravo <bravo.jose.luis18@gmail.com>
2020-07-01 23:31:37 -0300

    add yml

--- netcore-deploy-heroku/Program.cs
+++ netcore-deploy-heroku/Program.cs
@@ -12,18 +12,10 @@ namespace netcore_deploy_heroku
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(config)
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
